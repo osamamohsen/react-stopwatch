4a4e130b7c704c4b43428120ba550fa9a1f317fc
osamamohsen <osamamohsen1994@gmail.com>
2017-03-20 22:48:14 +0200

    finish stopwatch

--- index.android.js
+++ index.android.js
@@ -15,58 +15,87 @@ var StopWatch = React.createClass({
 
   getInitialState: function(){
     return {
-      timeElapsed: null
+      timeElapsed: null,
+      running: false,
+      startTime: null,
+      laps: [],
     }
   },
   render: function (){
     return <View style={styles.container}>
-      <View style={[styles.header,this.border('yellow')]}>
-        <View style={[styles.timerWrapper,this.border('red')]}>
-          <Text>
+      <View style={styles.header}>
+        <View style={styles.timerWrapper}>
+          <Text style={styles.timer}>
             {formatTime(this.state.timeElapsed)}
           </Text>
         </View>
-        <View style={[this.border('green'),styles.buttonWrapper]}>
+        <View style={styles.buttonWrapper}>
           {this.startStopButton()}
           {this.lapButton()}
         </View>
       </View>
-      <View style={[styles.footer,this.border('blue')]} >
-        <Text>
-          This Is List Laps
-        </Text>
+      <View style={styles.footer} >
+          {this.displayLaps()}
       </View>
     </View>
   },
+  displayLaps: function(){ // display array of laps
+    return this.state.laps.map(function(time,index){ // time: carry time , index: carry index
+      return <View  key={index} style={styles.lab}>
+        <Text style={styles.labText}>
+          #No of {index + 1}
+        </Text>
+        <Text style={styles.labText}>
+          {formatTime(time)}
+        </Text>
+      </View>
+    });
+  },
   startStopButton : function (){
+    var styleButton = this.state.running ? styles.stopButton : styles.startButton;
     return <TouchableHighlight
       underlayColor="gray"
       onPress={this.handleStartPress}
+      style={[styles.button , styleButton]}
       >
       <Text>
-        Start
+        { this.state.running ? 'Stop' : 'Start'}
       </Text>
     </TouchableHighlight>
   },
   lapButton: function(){
-    return <View>
+    return <TouchableHighlight
+       style={styles.button}
+       onPress={this.handleLapPress}
+       underlayColor="gray">
       <Text>
         Lap
       </Text>
-    </View>
+    </TouchableHighlight>
   },
-  border: function(color){
-    return {
-      borderWidth: 4,
-      borderColor: color,
-    }
+  handleLapPress: function(){ //handle-Lap-Press
+    var lap = this.state.timeElapsed;
+
+    this.setState({
+      startTime: new Date(),
+      laps: this.state.laps.concat([lap]), // concat => push element in array
+    });
   },
-  handleStartPress: function(){
+  handleStartPress: function(){ //handle-start-press
     var startTime = new Date();
 
-    setInterval(() => {
+    if(this.state.running){
+      clearInterval(this.interval); // clearInterval built in function clear interval
+      this.setState({running:false});
+      return; // timer is stop now
+    }
+
+    this.setState({startTime : new Date()});
+
+    this.interval = setInterval(() => {
       this.setState({
-        timeElapsed: new Date() - startTime
+        timeElapsed: new Date() - this.state.startTime ,
+        running: true,
       });
   } , 30); //run this function in every 30 mile-second
 
@@ -95,6 +124,30 @@ var styles = StyleSheet.create({
     flexDirection: 'row', // default is coulmn
     justifyContent: 'space-around', //put item in center as row by spacing between
     alignItems: 'center', //center item in div
+  },
+  timer: {
+    fontSize: 60,
+  },
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  startButton: {
+    borderColor: 'green',
+  },
+  stopButton: {
+    borderColor: 'red',
+  },
+  lab: {
+    justifyContent: 'space-around',
+    'flexDirection': 'row'
+  },
+  labText: {
+    fontSize: 30
   }
 });
 
